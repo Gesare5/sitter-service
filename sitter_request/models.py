652bda2ecfea9d0f0cd0c0d584d19ae128a2ea60
Gesare5 <gesare.nyangweso@gmail.com>
2024-09-17 14:30:31 +0300

    Change: Update SitterRequest model

--- sitter_request/models.py
+++ sitter_request/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 
+import uuid
+
 from sitter_requester.models import SitterRequester
-# Create your models here.
 
 class SitterRequest(models.Model):
-    request_id = models.CharField(max_length=200, null=True) #set as pk and use uuid
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+          primary_key=True, editable=False)
     service_type = models.CharField(max_length=100, null=True)
     service_date = models.DateTimeField(auto_now_add=True)
     town = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     sitter_requester_id = models.ForeignKey(SitterRequester, null=True, on_delete=models.SET_NULL)
-    attempt_sitter_pair_id = models.CharField(max_length=200)
-    paired_sitter_id = models.CharField(max_length=200)
+    attempt_sitter_pair_id = models.CharField(max_length=200)    #should be null
+    paired_sitter_id = models.CharField(max_length=200)           #should be null
     number_of_pets = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=0)
+    
+
 
 
 
