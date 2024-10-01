bbb761b422972ade5c58077769193536612d40bb
Gesare5 <gesare.nyangweso@gmail.com>
2024-10-01 09:43:01 +0300

    Change: Allow attempt_sitter_pair_id and paired_sitter_id to take on null values Change: Perform migrations

--- sitter_request/models.py
+++ sitter_request/models.py
@@ -21,13 +21,13 @@ class SitterRequest(models.Model):
     
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     service_type = models.IntegerField(choices=service_types, default=0)
-    service_date = models.DateTimeField(auto_now_add=True)
+    service_date = models.DateTimeField(auto_now_add=True)  #fix this
     status = models.IntegerField(choices=status_types, default=0)
     town = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     sitter_requester_id = models.ForeignKey(SitterRequester, null=True, on_delete=models.SET_NULL)
-    attempt_sitter_pair_id = models.CharField(max_length=200)    #should be null
-    paired_sitter_id = models.CharField(max_length=200)           #should be null
+    attempt_sitter_pair_id = models.CharField(max_length=200, null=True)   #should be integer field
+    paired_sitter_id = models.CharField(max_length=200, null=True)         #should be integer field  
     number_of_pets = models.IntegerField(default=1)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
